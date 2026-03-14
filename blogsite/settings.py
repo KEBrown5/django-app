110b17c3d7fd59a87933898116ba397ebb428282
Kyle <kbrown5work@gmail.com>
2026-03-13 22:12:51 -0400

    Changed order of cloudinary apps

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
     'cloudinary',
     'gallery',
     'users',
