afa6a0b15e55dc7348c3bf1781ef5ba0c49552a0
Kyle <kbrown5work@gmail.com>
2026-03-13 20:57:43 -0400

    fixed staticfiles_dirs variable

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -150,5 +150,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES__DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
